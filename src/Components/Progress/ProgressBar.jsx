31bb2e103d6bcfd023421b2e91b8a24937c3af75
amshal1 <panshal911@gmail.com>
2023-08-29 11:41:24 +0530

    progress bar UI chnaged

--- src/Components/Progress/ProgressBar.jsx
+++ src/Components/Progress/ProgressBar.jsx
@@ -1,13 +1,16 @@
 import React, { useContext } from "react";
 import Context from "../../Context/Context";
-import Loader from "../Loader/Loader"
+import Loader from "../Loader/Loader";
 import "./ProgressBar.css";
 const ProgressBar = () => {
-    const c = useContext(Context);
-    const {uploaded} = c
+  const c = useContext(Context);
+  const { uploaded } = c;
   return (
-    <div className="myprogress" style={{ display: `${uploaded.display}`}}>
-      <h1>{<Loader />}Uploading Please wait... <span>{uploaded.length}</span></h1>
+    <div className="myprogress" style={{ display: uploaded.display }}>
+      <h1>
+        {<Loader />}
+        <span> {uploaded.length}</span>
+      </h1>
     </div>
   );
 };
